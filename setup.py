582a233d6ea3ded1211faf49c5eb9da98f383e59
gavinlouuu <gavinlou@kingsphase.page>
2024-04-15 17:59:07 +0800

    package setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # dependencies
+    ],
+    python_requires='>=3.6',
+    author='gavinlouuu',
+    author_email='gavinlouuu@gmail.com',
+    description='A brief description of the package',
+    license='MIT',
+    keywords='example project',
+    url='http://example.com/official_project_url',
+)
